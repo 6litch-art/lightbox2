5114820da45ff536cf3e2d7eb6ae751b8a697f6b
Marco Meyer (www) <marco.meyerconde@gmail.com>
2022-11-28 03:09:28 +0100

    Update, cleanup

--- src/index.js
+++ src/index.js
@@ -1,46 +1,48 @@
+window.Lightbox = require('lightbox2');
+
 $(document).on("DOMContentLoaded.lightbox", function () {
 
     var original = {};
-
+    
     function args(_arguments) { return Array.prototype.slice.apply(_arguments); }
-    lightbox.trigger = function() { $(original).trigger.apply($(lightbox.lightbox), args(arguments)); };
-    lightbox.on = function() { $(original).on.apply($(lightbox.lightbox), args(arguments)); };
+    Lightbox.trigger = function() { $(original).trigger.apply($(Lightbox.lightbox), args(arguments)); };
+    Lightbox.on = function() { $(original).on.apply($(Lightbox.lightbox), args(arguments)); };
 
-    original['init'] = lightbox.init;
-    lightbox.init = function() {
+    original['init'] = Lightbox.init;
+    Lightbox.init = function() {
 
         var result = original['init'].apply(this, arguments);
         var _args = args(arguments);
             _args.unshift(result);
             _args.unshift(this);
 
-        lightbox.$container.trigger('onInit', _args);
+        Lightbox.$container.trigger('onInit', _args);
     };
     
-    original['start'] = lightbox.start;
-    lightbox.start = function() {
+    original['start'] = Lightbox.start;
+    Lightbox.start = function() {
 
         var result = original['start'].apply(this, arguments);
         var _args = args(arguments);
             _args.unshift(result);
             _args.unshift(this);
 
-        lightbox.$container.trigger('onStart', _args);
+        Lightbox.$container.trigger('onStart', _args);
     };
 
-    original['end'] = lightbox.end;
-    lightbox.end = function() {
+    original['end'] = Lightbox.end;
+    Lightbox.end = function() {
 
         var result = original['end'].apply(this, arguments);
         var _args = args(arguments);
             _args.unshift(result);
             _args.unshift(this);
 
-        lightbox.$container.trigger('onEnd', _args);
+        Lightbox.$container.trigger('onEnd', _args);
     };
 
-    original['changeImage'] = lightbox.changeImage;
-    lightbox.changeImage = function() {
+    original['changeImage'] = Lightbox.changeImage;
+    Lightbox.changeImage = function() {
 
         var _args = args(arguments);
             _args.unshift(this);
@@ -49,36 +51,36 @@ $(document).on("DOMContentLoaded.lightbox", function () {
         var result = original['changeImage'].apply(this, arguments);
 
         _args.unshift(result);
-        lightbox.$container.trigger('onChangeImage', _args);
+        Lightbox.$container.trigger('onChangeImage', _args);
     };
 
-    original['showImage'] = lightbox.showImage;
-    lightbox.showImage = function() {
+    original['showImage'] = Lightbox.showImage;
+    Lightbox.showImage = function() {
 
         var result = original['showImage'].apply(this, arguments);
         var _args = args(arguments);
             _args.unshift(result);
             _args.unshift(this);
 
-        lightbox.$container.trigger('onShowImage', _args);
+        Lightbox.$container.trigger('onShowImage', _args);
     };
 
-    original['sizeContainer'] = lightbox.sizeContainer;
-    lightbox.sizeContainer = function() {
+    original['sizeContainer'] = Lightbox.sizeContainer;
+    Lightbox.sizeContainer = function() {
 
         var result = original['sizeContainer'].apply(this, arguments);
         var _args = args(arguments);
             _args.unshift(result);
             _args.unshift(this);
 
-        lightbox.$container.trigger('onSizeContainer', _args);
+        Lightbox.$container.trigger('onSizeContainer', _args);
     };
 });
 
 $(window).on("load.ligthbox", function () {
 
-    if (lightbox.$container) {
-        lightbox.$container.on('onStart', (event, result, self) => $('html,body').css('overflow', 'hidden'));
-        lightbox.$container.on('onEnd'  , (event, result, self) => $('html,body').css('overflow', ''));
+    if (Lightbox.$container) {
+        Lightbox.$container.on('onStart', (event, result, self) => $('html,body').css('overflow', 'hidden'));
+        Lightbox.$container.on('onEnd'  , (event, result, self) => $('html,body').css('overflow', ''));
     }
 });
